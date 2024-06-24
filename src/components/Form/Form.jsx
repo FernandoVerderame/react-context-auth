cba941796c8c0fe661e6712899f197ac1944ad1b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-24 15:13:25 +0200

    added form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,177 @@
+// Importo lo style del Form, l'icona, la PostCard e lo useState
+import { useState } from 'react';
+import { useGlobal } from '../../contexts/GlobalContext';
+import formStyle from './Form.module.scss';
+
+const Form = ({ initialData, onSubmit }) => {
+
+    const { tags, categories } = useGlobal();
+
+    // Post di default
+    const defaultPostData = initialData || {
+        id: new Date().toISOString(),
+        title: '',
+        image: '',
+        content: '',
+        categoryId: '',
+        tags: [],
+        published: false
+    }
+
+
+    // useState del singolo nuovo Post
+    const [postData, setPostData] = useState(defaultPostData);
+
+    // Campo dei Tags
+    const handleField = (name, value) => {
+
+        setPostData(curr => ({
+            ...curr,
+            [name]: value
+        }));
+    }
+
+    // Submit del Form
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        onSubmit(postData);
+    }
+
+    // // Rimozione di un Post
+    // const removePost = (indexToDelete) => {
+    //     setPosts(array => array.filter((_, i) => i !== indexToDelete));
+    // }
+
+    // Update del Post
+    const changePostData = (key, newValue) => {
+        setPostData(data => ({ ...data, [key]: newValue }));
+    }
+
+    return (
+        <>
+            {/* Form Card */}
+            <div className={formStyle.cardForm}>
+
+                {/* Card Title */}
+                <div className={formStyle.cardTitle}>
+                    <h3>Creazione Post</h3>
+                </div>
+
+                {/* Card Body */}
+                <div className={formStyle.cardBody}>
+
+                    {/* Form */}
+                    <form onSubmit={handleSubmit}>
+
+                        <div className="row">
+                            <div className="col-6">
+                                {/* Input Title */}
+                                <div className={formStyle.cardInput}>
+                                    <label htmlFor="title">Titolo</label>
+                                    <input
+                                        type="text"
+                                        id='title'
+                                        name='title'
+                                        value={postData.title}
+                                        onChange={(e) => changePostData('title', e.target.value)}
+                                    />
+                                </div>
+
+                                {/* Input Image */}
+                                <div className={formStyle.cardInput}>
+                                    <label htmlFor="image">URL Immagine</label>
+                                    <input
+                                        type="url"
+                                        id='image'
+                                        name='image'
+                                        value={postData.image}
+                                        onChange={(e) => changePostData('image', e.target.value)}
+                                    />
+                                </div>
+
+                                {/* Input Content */}
+                                <div className={formStyle.cardInput}>
+                                    <label htmlFor="content">Content</label>
+                                    <textarea
+                                        id='content'
+                                        name='content'
+                                        rows='5'
+                                        value={postData.content}
+                                        onChange={(e) => changePostData('content', e.target.value)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="col-6">
+                                {/* Input Category */}
+                                <div className="mb-3">
+                                    <h3 className="h4">Categoria</h3>
+                                    <select
+                                        id="categoryId"
+                                        name="categoryId"
+                                        defaultValue={postData.categoryId}
+                                        onChange={(e) => changePostData('categoryId', e.target.value)}
+                                        className={formStyle.categorySelect}
+                                    >
+                                        <option value="">Seleziona una categoria</option>
+                                        {categories.map(c => (
+                                            <option
+                                                key={`categoryId${c.id}`}
+                                                value={c.id}>
+                                                {c.name}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+
+                                {/* Input Tags */}
+                                <div className="mb-5">
+                                    <h3 className="h4">Tags</h3>
+                                    <ul>
+                                        {tags.map((tag) => (
+                                            <li key={tag.id}>
+                                                <input
+                                                    type='checkbox'
+                                                    id={tag.id}
+                                                    name={tag.id}
+                                                    checked={postData.tags.includes(tag.id)}
+                                                    onChange={() => {
+                                                        const curr = postData.tags;
+                                                        const newTags = curr.includes(tag.id) ?
+                                                            curr.filter(el => el !== tag.id) :
+                                                            [...curr, tag.id];
+                                                        handleField('tags', newTags);
+                                                    }}
+                                                />
+                                                <label htmlFor={tag.id}>{tag.name}</label>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+
+                                <div className={formStyle.cardBtn}>
+
+                                    {/* Input Published */}
+                                    <div className={formStyle.published}>
+                                        <label htmlFor="published">Pubblicato</label>
+                                        <input
+                                            id='published'
+                                            name='published'
+                                            type='checkbox'
+                                            checked={postData['published']}
+                                            onChange={(e) => handleField('published', e.target.checked)}
+                                        />
+                                    </div>
+                                    <button className='btn btn-success'>Salva</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div >
+        </>
+    );
+}
+
+export default Form;
