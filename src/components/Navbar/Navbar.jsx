fdb99fada0c0943aa5c78c66052a42fc2d81ec29
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-25 09:06:21 +0200

    added login/logout buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 const Navbar = () => {
+
+    const { isLoggedIn, logout } = useAuth();
+
     return (
-        <nav className="navbar navbar-expand-lg bg-secondary-subtle">
+        <nav className="navbar navbar-expand-lg bg-secondary-subtle px-5">
             <div className="container-fluid">
                 <NavLink className="navbar-brand" to={'/'}>Blog</NavLink>
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -13,6 +17,12 @@ const Navbar = () => {
                         <NavLink className="nav-link" to={'/posts'}>Posts</NavLink>
                     </li>
                 </ul>
+                {!isLoggedIn &&
+                    <NavLink to={`/login`} className="btn btn-primary">Login</NavLink>
+                }
+                {isLoggedIn &&
+                    <button onClick={logout} className="btn btn-secondary">Logout</button>
+                }
             </div>
         </nav>
     );
