17b27f99c18acfa4340b6c527e9f03c86f9fef56
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-25 09:06:55 +0200

    added public and private routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,45 @@ import PostDetail from "./pages/PostDeatil.jsx";
 import CreatePost from "./pages/CreatePost.jsx";
 import { GlobalProvider } from "./contexts/GlobalContext.jsx";
 import EditPost from "./pages/EditPost.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import PrivatePage from "./middlewares/PrivatePage.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalProvider>
-        <Routes>
-          <Route path="/" element={<DefaultLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="posts">
-              <Route index element={<Posts />} />
-              <Route path=":slug" >
-                <Route index element={<PostDetail />} />
-                <Route path="edit" element={<EditPost />} />
-              </ Route>
-              <Route path="create" element={<CreatePost />} />
+        <AuthProvider>
+          <Routes>
+
+            {/* Rotte pubbliche */}
+            <Route path="/" element={<DefaultLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="login" element={<Login />} />
+              <Route path="posts">
+                <Route index element={<Posts />} />
+                <Route path=":slug" >
+                  <Route index element={<PostDetail />} />
+                </ Route>
+              </Route>
             </Route>
-          </Route>
-        </Routes>
+
+            {/* Rotte private */}
+            <Route path="/" element={
+              <PrivatePage>
+                <DefaultLayout />
+              </PrivatePage>
+            }>
+              <Route path="posts">
+                <Route path=":slug">
+                  <Route path="edit" element={<EditPost />} />
+                </Route>
+                <Route path="create" element={<CreatePost />} />
+              </Route>
+            </Route>
+
+          </Routes>
+        </AuthProvider>
       </GlobalProvider>
     </BrowserRouter>
   );
